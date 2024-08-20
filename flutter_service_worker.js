de0be3f5209dfdf8e4f158053dfcf1a9a46d5e99
DESKTOP-DPLIS5K\Sung H Kim <hoony0916@gmail.com>
2024-08-20 12:13:56 +0900

    V1.7.1 beta : 홈페이지 테마 설정기능 추가

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -63,11 +63,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "2bf27a5af67c0381b99ad85adcc152b7
 "icons/Icon-512.png": "9ef998aa2174bd8255689837596a25a9",
 "icons/Icon-maskable-192.png": "edd7bee2902b700b96963bef12bf8b75",
 "icons/Icon-maskable-512.png": "9ef998aa2174bd8255689837596a25a9",
-"index.html": "b6ca5c1984a7db9c0b29ce824a0eaa63",
-"/": "b6ca5c1984a7db9c0b29ce824a0eaa63",
-"main.dart.js": "fe60db34d4dfa80d121c5eb9d18ec0cd",
+"index.html": "4058856e2a1201889316ca417deb002c",
+"/": "4058856e2a1201889316ca417deb002c",
+"main.dart.js": "cf2ea8dacdce357d32066757b09972ce",
 "manifest.json": "cdac920e092974e26d0a19f7c7402efa",
-"version.json": "cf4ef20c6faf851a29971c96820c7c8d"};
+"version.json": "d7f1c1ab07cb5e0bc9bb7114a4c8025a"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
